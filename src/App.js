f94b5f37b91487734dc0b34aaa043c1b57cf8e1f
MturkMturk <mturk1mturk@gmail.com>
2021-04-15 15:18:58 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -11,6 +11,15 @@ class App extends Component {
         <div className="App">
           <Switch>
               <Route exact path='/one' component={One} />
+              <Route exact path='/two' component={Two} />
+              <Route exact path='/three' component={Three} />
+              <Route exact path='/four' component={Four} />
+              <Route exact path='/five' component={Five} />
+              <Route exact path='/six' component={Six} />
+              <Route exact path='/seven' component={Seven} />
+              <Route exact path='/eight' component={Eight} />
+              <Route exact path='/nine' component={Nine} />
+              <Route exact path='/ten' component={Ten} />
           </Switch>
         </div>
       </Router>
