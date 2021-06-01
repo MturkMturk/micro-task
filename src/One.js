b8b12c3a60ed7afd062c40b09e5f3c7a24d456e2
MturkMturk <31360631+MturkMturk@users.noreply.github.com>
2021-06-01 02:36:45 -0400

    Update One.js

--- src/One.js
+++ src/One.js
@@ -7,7 +7,7 @@ class One extends Component{
     return (
       <div>
         <Iframe
-          url="https://cdn.jwplayer.com/players/shAkFfZ5-PzqYXf16.html"
+          url="https://cdn.jwplayer.com/players/CKdDnwqZ-PzqYXf16.html"
           width="450px"
           height="450px"
           id="myId"
