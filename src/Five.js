1b1ddb0497dd6611e77f3ef667d217b21b5a9bd5
MturkMturk <31360631+MturkMturk@users.noreply.github.com>
2021-08-25 07:51:55 -0400

    Update Five.js

--- src/Five.js
+++ src/Five.js
@@ -7,7 +7,7 @@ class Five extends Component{
     return (
       <div>
         <Iframe
-          url="https://cdn.jwplayer.com/players/ViwiDyQW-PzqYXf16.html"
+          url="https://cdn.jwplayer.com/players/wxcHPN97-PzqYXf16.html"
           width="450px"
           height="450px"
           id="myId"
