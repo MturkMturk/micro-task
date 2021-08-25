7e3607064d398714c736bd2d6ba63cc4e3d2f083
MturkMturk <31360631+MturkMturk@users.noreply.github.com>
2021-08-25 07:50:09 -0400

    Update Two.js

--- src/Two.js
+++ src/Two.js
@@ -7,7 +7,7 @@ class Two extends Component{
     return (
       <div>
         <Iframe
-          url="https://cdn.jwplayer.com/players/BVqJAfdC-PzqYXf16.html"
+          url="https://cdn.jwplayer.com/players/qhYiOgfy-PzqYXf16.html"
           width="450px"
           height="450px"
           id="myId"
