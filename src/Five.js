f76ba1d3331402c1ea0c5917909d2123ab043a88
MturkMturk <31360631+MturkMturk@users.noreply.github.com>
2022-10-29 00:07:03 -0400

    Update Five.js

--- src/Five.js
+++ src/Five.js
@@ -3,8 +3,9 @@ import React, { Component } from "react"
 import Iframe from "./iframe.js"
 
 class Five extends Component{
-  console.log("Hello World");
+
   render(){
+    console.log("Hello World");
     return (
       <div>
         <Iframe
