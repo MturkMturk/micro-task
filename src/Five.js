f8c9320c564d616c3719beb9cb4afe58cf9cc741
MturkMturk <31360631+MturkMturk@users.noreply.github.com>
2024-06-03 01:00:08 -0400

    Update Five.js

--- src/Five.js
+++ src/Five.js
@@ -1,25 +1,59 @@
+import React, { Component } from "react";
+import Iframe from "./iframe";
 import './App.css';
-import React, { Component } from "react"
-import Iframe from "./iframe.js"
 
-class Five extends Component{
+class Five extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      password: "",
+      hasAccess: false,
+    };
+  }
 
-  render(){
-    console.log("Hello World");
-    return (
-      <div>
-        <Iframe
-          url="https://cdn.jwplayer.com/players/m7DV5nAS-PzqYXf16.html"
-          width="450px"
-          height="450px"
-          id="myId"
-          className="myClassname"
-          display="initial"
-          position="relative"
-          allowFullScreen
-        />
-      </div>
-    );
+  handlePasswordChange = (event) => {
+    this.setState({ password: event.target.value });
+  };
+
+  checkPassword = () => {
+    const correctPassword = "hardcoded"; // Replace with your password
+    if (this.state.password === correctPassword) {
+      this.setState({ hasAccess: true });
+    } else {
+      alert("Incorrect password. Please try again.");
+    }
+  };
+
+  render() {
+    if (this.state.hasAccess) {
+      return (
+        <div>
+          <Iframe
+            url="https://cdn.jwplayer.com/players/HXtdRpnG-PzqYXf16.html"
+            width="450px"
+            height="450px"
+            id="myId"
+            className="myClassname"
+            display="initial"
+            position="relative"
+            allowFullScreen
+          />
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h2>Please enter the password to view the content</h2>
+          <input
+            type="password"
+            value={this.state.password}
+            onChange={this.handlePasswordChange}
+            placeholder="Enter password"
+          />
+          <button onClick={this.checkPassword}>Submit</button>
+        </div>
+      );
+    }
   }
 }
 
