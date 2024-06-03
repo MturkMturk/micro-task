5de3bb65a5fa2c7beeddddc809e35703b4cc45e6
MturkMturk <31360631+MturkMturk@users.noreply.github.com>
2024-06-03 11:32:52 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,32 @@
 import './App.css';
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import One from './One'
-import Two from './Two'
-import Three from './Three'
-import Four from './Four'
-import Five from './Five'
-import Six from './Six'
-import Seven from './Seven'
-import Eight from './Eight'
-import Nine from './Nine'
-import Ten from './Ten'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import One from './One';
+import Two from './Two';
+import Three from './Three';
+import Four from './Four';
+import Five from './Five';
+import Six from './Six';
+import Seven from './Seven';
+import Eight from './Eight';
+import Nine from './Nine';
+import Ten from './Ten';
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <Switch>
-              <Route exact path='/one' component={One} />
-              <Route exact path='/two' component={Two} />
-              <Route exact path='/three' component={Three} />
-              <Route exact path='/four' component={Four} />
-              <Route exact path='/five' component={Five} />
-              <Route exact path='/six' component={Six} />
-              <Route exact path='/seven' component={Seven} />
-              <Route exact path='/eight' component={Eight} />
-              <Route exact path='/nine' component={Nine} />
-              <Route exact path='/ten' component={Ten} />
-          </Switch>
-        </div>
-      </Router>
-      
-    );
-  }
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/one' element={<One />} />
+      <Route path='/two' element={<Two />} />
+      <Route path='/three' element={<Three />} />
+      <Route path='/four' element={<Four />} />
+      <Route path='/five' element={<Five />} />
+      <Route path='/six' element={<Six />} />
+      <Route path='/seven' element={<Seven />} />
+      <Route path='/eight' element={<Eight />} />
+      <Route path='/nine' element={<Nine />} />
+      <Route path='/ten' element={<Ten />} />
+    </Routes>
+  );
 }
 
 export default App;
